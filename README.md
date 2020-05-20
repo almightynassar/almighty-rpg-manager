a927950b071f8bcbf81ff44f7842342915a9263f
Nassar Zeitoune <nassar.z@slair.com.au>
2020-05-20 20:49:47 +1000

    [feat] Added leveling, and a better introduction with links to more resources

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 # Almighty RPG Manager (almighty-rpg-manager)
 
-RPG Management App.
+![Upload Release](https://github.com/almightynassar/almighty-rpg-manager/workflows/Upload%20Release/badge.svg)
 
-This application uses [Quasar](https://quasar.dev/).
+This is an RPG Management App. It is geared for D&D5E and a homebrew campaign, but can be edited via the files located in `src/assets/data` on the [GitHub page](https://github.com/almightynassar/almighty-rpg-manager).
+
+Some useful links and resources:
+
+- [A easy to use Player reference webpage](https://crobi.github.io/dnd5e-quickref/preview/quickref.html)
+- [The D&D Compendium, a massive resource](https://www.dnd-compendium.com/)
+- [A flexible roll-table that covers a lot of bases](http://autorolltables.github.io/#)
+- [Dave's mapper, a simple to use battle-map generator](https://davesmapper.com/)
+- [donjon.sh, a premier source of random generators and DM resources](https://donjon.bin.sh/)
+- [Homebrewery, where you can make your homebrew look official](https://homebrewery.naturalcrit.com/)
+- [Inkarnate, a really nice map maker](https://inkarnate.com/login/)
+- [Wikibox, which makes wikipedia style battle summaries](http://n.bellok.de/wikibox/)
+- [A big Worldbuilding resource compiled by Kenneth Jorgensen](http://kennethjorgensen.com/worldbuilding/resources)
+- [City generator software](https://watabou.itch.io/medieval-fantasy-city-generator/devlog/85275/070-districts)
+- [Fantasy world map generator](https://azgaar.github.io/Fantasy-Map-Generator/)
+- [5E tools, which is this manager but more comprehensive. Not sure about the legality, though...](https://5e.tools/)
 
 ## Getting Start
 Install the dependencies:
@@ -25,8 +40,7 @@ To Build the app for production:
 quasar build
 ```
 
-## Customize the configuration
-See [Configuring quasar.conf.js](https://quasar.dev/quasar-cli/quasar-conf-js).
+This application uses [Quasar](https://quasar.dev/). To customise the settings, zee [Configuring quasar.conf.js](https://quasar.dev/quasar-cli/quasar-conf-js).
 
 ## Resources
 
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,21 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>Almighty RPG Manager</h1>
+  <q-page class="q-pa-md">
+    <p>This is an RPG Management App. It is geared for D&D5E and a homebrew campaign, but can be edited via the files located in <span class="text-primary">src/assets/data</span> on the <a target="_blank" href="https://github.com/almightynassar/almighty-rpg-manager">GitHub page</a>.
+    <p>Some useful links and resources:</p>
+    <ul>
+      <li><a target="_blank" href="https://crobi.github.io/dnd5e-quickref/preview/quickref.html">A easy to use Player reference webpage</a></li>
+      <li><a target="_blank" href="https://www.dnd-compendium.com/">The D&D Compendium, a massive resource</a></li>
+      <li><a target="_blank" href="http://autorolltables.github.io/#">A flexible roll-table that covers a lot of bases</a></li>
+      <li><a target="_blank" href="https://davesmapper.com/">Dave's mapper, a simple to use battle-map generator</a></li>
+      <li><a target="_blank" href="https://donjon.bin.sh/">donjon.sh, a premier source of random generators and DM resources</a></li>
+      <li><a target="_blank" href="https://homebrewery.naturalcrit.com/">Homebrewery, where you can make your homebrew look official</a></li>
+      <li><a target="_blank" href="https://inkarnate.com/login/">Inkarnate, a really nice map maker</a></li>
+      <li><a target="_blank" href="http://n.bellok.de/wikibox/">Wikibox, which makes wikipedia style battle summaries</a></li>
+      <li><a target="_blank" href="http://kennethjorgensen.com/worldbuilding/resources">A big Worldbuilding resource compiled by Kenneth Jorgensen</a></li>
+      <li><a target="_blank" href="https://watabou.itch.io/medieval-fantasy-city-generator/devlog/85275/070-districts">City generator software</a></li>
+      <li><a target="_blank" href="https://azgaar.github.io/Fantasy-Map-Generator/">Fantasy world map generator</a></li>
+      <li><a target="_blank" href="https://5e.tools/">5E tools, which is this manager but more comprehensive. Not sure about the legality, though...</a></li>
+    </ul>
   </q-page>
 </template>
 
