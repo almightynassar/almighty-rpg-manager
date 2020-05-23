e551dcfc963135afc34dea47190d5d820ed5bb9c
Nassar Zeitoune <nassar.z@slair.com.au>
2020-05-23 21:33:33 +1000

    [feat] Added tools and entertainment items

--- src/assets/data/gear/list.js
+++ src/assets/data/gear/list.js
@@ -1,8 +1,10 @@
 import Clothing from './section/clothing'
 import Creature from './section/creature'
 import Edible from './section/edible'
+import Entertainment from './section/entertainment'
 import Furnishing from './section/furnishing'
 import General from './section/general'
+import Tools from './section/tools'
 import Trade from './section/trade'
 import Warfare from './section/warfare'
 
@@ -10,8 +12,10 @@ export default [
   ...Clothing,
   ...Creature,
   ...Edible,
+  ...Entertainment,
   ...Furnishing,
   ...General,
+  ...Tools,
   ...Trade,
   ...Warfare
 ]
