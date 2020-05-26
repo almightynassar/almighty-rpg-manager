1849aefee3d111c995e2e5cabea8c0edf2e78137
Nassar Zeitoune <nassar.z@slair.com.au>
2020-05-27 03:27:05 +1000

    [fix] All name sets are collected in one file

--- src/pages/NameGenerator.vue
+++ src/pages/NameGenerator.vue
@@ -51,80 +51,14 @@
 </template>
 
 <script >
-import Akkadian from 'src/assets/data/names/akkadian'
-import AztecFemale from 'src/assets/data/names/aztec-female'
-import AztecMale from 'src/assets/data/names/aztec-male'
-import ChineseFemale from 'src/assets/data/names/chinese-female'
-import ChineseMale from 'src/assets/data/names/chinese-male'
-import DutchFemale from 'src/assets/data/names/dutch-female'
-import DutchMale from 'src/assets/data/names/dutch-male'
-import FrenchFemale from 'src/assets/data/names/french-female'
-import FrenchMale from 'src/assets/data/names/french-male'
-import GermanicFemale from 'src/assets/data/names/germanic-female'
-import GermanicMale from 'src/assets/data/names/germanic-male'
-import HinduFemale from 'src/assets/data/names/hindu-female'
-import HinduMale from 'src/assets/data/names/hindu-male'
-import HungarianFemale from 'src/assets/data/names/hungarian-female'
-import HungarianMale from 'src/assets/data/names/hungarian-male'
-import IrishFemale from 'src/assets/data/names/irish-female'
-import IrishMale from 'src/assets/data/names/irish-male'
-import ItalianFemale from 'src/assets/data/names/italian-female'
-import ItalianMale from 'src/assets/data/names/italian-male'
-import JapaneseFemale from 'src/assets/data/names/japanese-female'
-import JapaneseMale from 'src/assets/data/names/japanese-male'
-import NativeAmericanFemale from 'src/assets/data/names/native-american-female'
-import NativeAmericanMale from 'src/assets/data/names/native-american-male'
-import ScandinavianFemale from 'src/assets/data/names/scandinavian-female'
-import ScandinavianMale from 'src/assets/data/names/scandinavian-male'
-import ScottishFemale from 'src/assets/data/names/scottish-female'
-import ScottishMale from 'src/assets/data/names/scottish-male'
-import SpanishFemale from 'src/assets/data/names/spanish-female'
-import SpanishMale from 'src/assets/data/names/spanish-male'
-import SwedishFemale from 'src/assets/data/names/swedish-female'
-import SwedishMale from 'src/assets/data/names/swedish-male'
-import WelshFemale from 'src/assets/data/names/welsh-female'
-import WelshMale from 'src/assets/data/names/welsh-male'
+import Names from 'src/assets/data/Names'
 import Markov from 'src/js/markov'
 
 export default {
   name: 'NamesList',
   data: function () {
     return {
-      sets: {
-        akkadian: Akkadian,
-        aztecFemale: AztecFemale,
-        aztecMale: AztecMale,
-        chineseFemale: ChineseFemale,
-        chineseMale: ChineseMale,
-        dutchFemale: DutchFemale,
-        dutchMale: DutchMale,
-        frenchFemale: FrenchFemale,
-        frenchMale: FrenchMale,
-        germanicFemale: GermanicFemale,
-        germanicMale: GermanicMale,
-        hinduFemale: HinduFemale,
-        hinduMale: HinduMale,
-        hungarianFemale: HungarianFemale,
-        hungarianMale: HungarianMale,
-        irishFemale: IrishFemale,
-        irishMale: IrishMale,
-        italianFemale: ItalianFemale,
-        italianMale: ItalianMale,
-        japaneseFemale: JapaneseFemale,
-        japaneseMale: JapaneseMale,
-        nativeAmericanFemale: NativeAmericanFemale,
-        nativeAmericanMale: NativeAmericanMale,
-        scandinavianFemale: ScandinavianFemale,
-        scandinavianMale: ScandinavianMale,
-        scottishFemale: ScottishFemale,
-        scottishMale: ScottishMale,
-        spanishFemale: SpanishFemale,
-        spanishMale: SpanishMale,
-        swedishFemale: SwedishFemale,
-        swedishMale: SwedishMale,
-        welshFemale: WelshFemale,
-        welshMale: WelshMale
-      },
+      sets: Names,
       selected: 'germanicFemale',
       names: [],
       options: [],
