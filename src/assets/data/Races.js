ae2305398203047ed6ebb7e75ba92c71a4beabc7
Nassar Zeitoune <nassar.z@slair.com.au>
2020-05-27 04:10:28 +1000

    [feat] Added Mermen and Lacerti

--- src/assets/data/Races.js
+++ src/assets/data/Races.js
@@ -1,13 +1,17 @@
 import Avian from './races/avians'
 import Dwarf from './races/dwarves'
 import Human from './races/humans'
+import Lacerti from './races/lacerti'
 import Lefantian from './races/lefantians'
 import Locanth from './races/locanth'
+import Mermen from './races/mermen'
 
 export default [
   Avian,
   Dwarf,
   Human,
+  Lacerti,
   Lefantian,
-  Locanth
+  Locanth,
+  Mermen
 ]
