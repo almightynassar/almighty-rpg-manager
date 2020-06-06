01a8d3458ba9b620ea465142e116a545fa7a093d
Nassar Zeitoune <almightynassar@gmail.com>
2020-06-07 03:10:08 +1000

    Added some new items

--- src/assets/data/gear/section/general.js
+++ src/assets/data/gear/section/general.js
@@ -4,7 +4,11 @@ export default [
   { name: 'Barrel', type: 'General', cost: 200, weight: 8, properties: [], acdamage: '', effects: [], requirements: [], description: 'A wooden barrel that holds 150 litres of liquid' },
   { name: 'Bedroll', type: 'General', cost: 100, weight: 3.5, properties: [], acdamage: '', effects: [], requirements: [], description: 'A simple bedroll made out of fabric and stuffing' },
   { name: 'Bell', type: 'General', cost: 100, weight: 0.01, properties: [], acdamage: '', effects: [], requirements: [], description: 'A small metal bell' },
+  { name: 'Blanket', type: 'General', cost: 50, weight: 1.5, properties: [], acdamage: '', effects: [], requirements: [], description: 'A warm sheet of fabric, used for bedding' },
+  { name: 'Bomb', type: 'General', cost: 15000, weight: 1.5, properties: [], acdamage: '', effects: ['As an action, a character can light this bomb and throw it at a point up to 18m away. Each creature within 1.5m of that point must succeed on a DC 12 DEX saving throw or take 3d6 fire damage.'], requirements: ['Max. 4'], description: 'An explosive concoction that is contained in metal. Can only safely carry 4 at a time.' },
   { name: 'Bucket', type: 'General', cost: 5, weight: 1.5, properties: [], acdamage: '', effects: [], requirements: [], description: 'A wooden bucket that holds 10 litres of liquid' },
+  { name: 'Hammer', type: 'General', cost: 100, weight: 1.5, properties: [], acdamage: '', effects: [], requirements: [], description: 'A tool used from hammering' },
+  { name: 'Healing Kit', type: 'General', cost: 500, weight: 1.5, properties: [], acdamage: '', effects: ['As an action, you can expend one use of the kit to stabilize a creature that has 0 hit points, without needing to make a Wisdom (Medicine) check.'], requirements: [], description: 'This kit is a leather pouch containing bandages, salves, and splints. The kit has ten uses.' },
   { name: 'Ink', type: 'General', cost: 400, weight: 0.01, properties: [], acdamage: '', effects: [], requirements: [], description: 'The listed price is for black ink. Double the cost for coloured inks.' },
   { name: 'Parchment', type: 'General', cost: 20, weight: 0.01, properties: [], acdamage: '', effects: [], requirements: [], description: 'A prepared piece of hide fro writing on' },
   { name: 'Paper', type: 'General', cost: 40, weight: 0.01, properties: [], acdamage: '', effects: [], requirements: [], description: 'A white sheet of paper made from cloth fibres' },
@@ -13,7 +17,9 @@ export default [
   { name: 'Lock - Advanced', type: 'General', cost: 4000, weight: 0.5, properties: [], acdamage: '', effects: ['Can be unlocked without a key by using theives tools and successfully passing a DC 20 check'], requirements: [], description: 'A key is provided with the lock. Provides better security than a normal lock' },
   { name: 'Lock', type: 'General', cost: 1000, weight: 0.5, properties: [], acdamage: '', effects: ['Can be unlocked without a key by using theives tools and successfully passing a DC 15 check'], requirements: [], description: 'A key is provided with the lock' },
   { name: 'Sack', type: 'General', cost: 1, weight: 0.25, properties: [], acdamage: '', effects: [], requirements: [], description: 'A fabric sack that holds roughly 20kg of gear' },
+  { name: 'Sledgehammer', type: 'General', cost: 200, weight: 5, properties: [], acdamage: '', effects: [], requirements: [], description: 'A large hammer' },
   { name: 'Torch', type: 'General', cost: 1, weight: 0.5, properties: [], acdamage: '', effects: [], requirements: [], description: 'A torch burns for 1 hour, providing bright light in a 6m (4 square) radius and dim light for an additional 6m. If an attack with a torch hits, deal 1 fire damage' },
+  { name: 'Vial (empty)', type: 'General', cost: 100, weight: 0.01, properties: [], acdamage: '', effects: [], requirements: [], description: 'An empty glass vial' },
   { name: 'Waterskin', type: 'General', cost: 20, weight: 2.5, properties: [], acdamage: '', effects: [], requirements: [], description: 'A liquid container made from waterproof fabric' },
   { name: 'Whetstone', type: 'General', cost: 1, weight: 0.01, properties: [], acdamage: '', effects: [], requirements: [], description: 'A whetstone is a stone used to sharpen weapons and blades' }
 ]
