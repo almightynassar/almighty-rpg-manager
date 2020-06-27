e2fb8ac31a8e5cdb741def06ced82755b8fa967a
Nassar Zeitoune <nassar.z@slair.com.au>
2020-06-27 23:27:30 +1000

    Finished the written age

--- src/assets/data/setting/History.js
+++ src/assets/data/setting/History.js
@@ -3,11 +3,13 @@ import Gauldan from './histories/gauldan'
 import Essence from './histories/essence'
 import Sundering from './histories/sundering'
 import Rebirth from './histories/rebirth'
+import Written from './histories/written'
 
 export default [
   Realms,
   Gauldan,
   Essence,
   Sundering,
-  Rebirth
+  Rebirth,
+  Written
 ]
