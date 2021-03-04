b2b273cd5b662fea3d38cc7315c4a2ccc5ba20c3
Nassar Zeitoune <nassar.z@slair.com.au>
2021-03-05 02:22:13 +1100

    Dynamic importing

--- src/boot/nations.js
+++ src/boot/nations.js
@@ -1,76 +1,31 @@
-// Nations
-import Federation from 'src/assets/data/nations/federation'
-import Hentergrey from 'src/assets/data/nations/hentergrey'
-import IllumiNaati from 'src/assets/data/nations/illuminaati'
-import Myrmiddon from 'src/assets/data/nations/myrmiddon'
-import OcearusNation from 'src/assets/data/nations/ocearus'
-import Partisans from 'src/assets/data/nations/partisans'
-import ShatteredIsles from 'src/assets/data/nations/shatteredisles'
-import SnvertNation from 'src/assets/data/nations/snvert'
-import Tau from 'src/assets/data/nations/tau'
-import TrellisantheNation from 'src/assets/data/nations/trellisanthe'
+const nations = []
+const cities = []
 
-// Cities
-import AlKhaleera from 'src/assets/data/cities/al-khaleera'
-import Amphirus from 'src/assets/data/cities/amphirus'
-import Angarth from 'src/assets/data/cities/angarth'
-import Arkaxus from 'src/assets/data/cities/arkaxus'
-import Berylnn from 'src/assets/data/cities/berylnn'
-import Caelan from 'src/assets/data/cities/caelan'
-import Deadrum from 'src/assets/data/cities/deadrum'
-import Deaton from 'src/assets/data/cities/deaton'
-import Dessingrove from 'src/assets/data/cities/dessingrove'
-import Hirst from 'src/assets/data/cities/hirst'
-import FreeMason from 'src/assets/data/cities/freemason'
-import Kaytoo from 'src/assets/data/cities/kaytoo'
-import Kelerak from 'src/assets/data/cities/kelerak'
-import Permand from 'src/assets/data/cities/permand'
-import Phalanxis from 'src/assets/data/cities/phalanxis'
-import OcearusCity from 'src/assets/data/cities/ocearus'
-import Sacium from 'src/assets/data/cities/sacium'
-import SnvertCity from 'src/assets/data/cities/snvert'
-import Torvia from 'src/assets/data/cities/torvia'
-import TradeTown from 'src/assets/data/cities/tradetown'
-import TrellisantheCity from 'src/assets/data/cities/trellisanthe'
-import Walla from 'src/assets/data/cities/walla'
+require.context('src/assets/data/nations', true, /\.js$/)
+  .keys()
+  .filter(page => page.split('/').length >= 2)
+  .map(page => page.slice(2).slice(0, -3))
+  .forEach(file => {
+    import('src/assets/data/nations/' + file)
+      .then((module) => {
+        nations.push(module.default)
+      })
+  })
+
+require.context('src/assets/data/cities', true, /\.js$/)
+  .keys()
+  .filter(page => page.split('/').length >= 2)
+  .map(page => page.slice(2).slice(0, -3))
+  .forEach(file => {
+    import('src/assets/data/cities/' + file)
+      .then((module) => {
+        cities.push(module.default)
+      })
+  })
 
 export default ({ Vue }) => {
   Vue.prototype.$nations = {
-    cities: [
-      AlKhaleera,
-      Amphirus,
-      Angarth,
-      Arkaxus,
-      Berylnn,
-      Caelan,
-      Deadrum,
-      Deaton,
-      Dessingrove,
-      Hirst,
-      FreeMason,
-      Kaytoo,
-      Kelerak,
-      OcearusCity,
-      Permand,
-      Phalanxis,
-      Sacium,
-      SnvertCity,
-      Torvia,
-      TradeTown,
-      TrellisantheCity,
-      Walla
-    ],
-    nations: [
-      Federation,
-      Hentergrey,
-      IllumiNaati,
-      Myrmiddon,
-      OcearusNation,
-      Partisans,
-      ShatteredIsles,
-      SnvertNation,
-      Tau,
-      TrellisantheNation
-    ]
+    cities: cities,
+    nations: nations
   }
 }
--- src/boot/races.js
+++ src/boot/races.js
@@ -1,37 +1,18 @@
 import RaceTraits from 'src/assets/data/RaceTraits'
-import Avians from 'src/assets/data/races/avians'
-import Dwarves from 'src/assets/data/races/dwarves'
-import Forged from 'src/assets/data/races/forged'
-import Goblins from 'src/assets/data/races/goblins'
-import Humans from 'src/assets/data/races/humans'
-import Lacerti from 'src/assets/data/races/lacerti'
-import Lefantians from 'src/assets/data/races/lefantians'
-import Locanth from 'src/assets/data/races/locanth'
-import Merfolk from 'src/assets/data/races/merfolk'
-import Minotaurs from 'src/assets/data/races/minotaurs'
-import Orcs from 'src/assets/data/races/orcs'
-import Snvert from 'src/assets/data/races/snvert'
-import Shifters from 'src/assets/data/races/shifters'
-import Teztecs from 'src/assets/data/races/teztecs'
-
+const races = []
+require.context('src/assets/data/races', true, /\.js$/)
+  .keys()
+  .filter(page => page.split('/').length >= 2)
+  .map(page => page.slice(2).slice(0, -3))
+  .forEach(file => {
+    import('src/assets/data/races/' + file)
+      .then((module) => {
+        races.push(module.default)
+      })
+  })
 export default ({ Vue }) => {
   Vue.prototype.$races = {
     traits: RaceTraits,
-    races: [
-      Avians,
-      Dwarves,
-      Forged,
-      Goblins,
-      Humans,
-      Lacerti,
-      Lefantians,
-      Locanth,
-      Merfolk,
-      Minotaurs,
-      Orcs,
-      Snvert,
-      Shifters,
-      Teztecs
-    ]
+    races: races
   }
 }
